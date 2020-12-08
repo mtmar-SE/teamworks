8863051ac96f1b299d6babb1e78ec0aa7f09420b
ShibuyaYuki <96yukiyuki@gmail.com>
2020-12-08 22:44:01 +0900

    recommendの追加

--- src/components/Recommend.js
+++ src/components/Recommend.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Recommend = () => {
-  return <div>recommned</div>;
+  return (
+    <div className='recommend'>
+      <h2>おすすめ</h2>
+      <h3>渋谷</h3>
+      <iframe title='video' width="560" height="315" src="https://www.youtube.com/embed/Otrc2zAlJyM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <h3>一ノ瀬</h3>
+      <iframe title='video' width="560" height="315" src="https://www.youtube.com/embed/yECZUUhljjg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <h3>平沢</h3>
+      <iframe title='video' width="560" height="315" src="https://www.youtube.com/embed/pnsieVYy72M" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  )
 };
 
 export default Recommend;
